b950cbe6907a601b75c958cb1dd5528afcd3ca19
unknown <mjrajak47z@gmail.com>
2025-02-04 23:41:25 +0530

    assigment done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, {useState, useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./weatherDisplay";
 
 const App = () => {
+
+  const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
+
+  useEffect(() => {
+    // Updating the weather data as per the given input
+    setWeather({ temperature: 25, conditions: "Sunny" });
+  }, []);
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+
+        <h2>Conditional Weather Report</h2>
+        <WeatherDisplay weather={weather} />
     </div>
   )
 }
